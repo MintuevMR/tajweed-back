6f18e8d6174deac06c21f896d94aa87923c65b50
Mintuev <mail@example.com>
2023-08-01 11:11:29 +0300

    валидация имен

--- validation/auth.validator.js
+++ validation/auth.validator.js
@@ -3,8 +3,8 @@ import {body} from "express-validator"
 export const registerValidation = [
     body('login', "Логин должен содержать минимум 5 символов").isLength({min : 5}),
     body('password', "Пароль должен быть минимум 5 символов").isLength({min : 5}),
-    body('firstName', "Укажите имя").isLength({min : 5}),
-    body('lastName', "Укажите фамилию").isLength({min : 5}),
+    body('firstName', "Укажите имя").isLength({min : 2}).isString(),
+    body('lastName', "Укажите фамилию").isLength({min : 2}).isString(),
 ]
 
 export const loginValidation = [
@@ -14,6 +14,6 @@ export const loginValidation = [
 
 
 export const changeInfoValidation = [
-    body('firstName', "Укажите имя").isLength({min : 5}),
-    body('lastName', "Укажите фамилию").isLength({min : 5}),
+    body('firstName', "Укажите имя").isLength({min : 2}).isString(),
+    body('lastName', "Укажите фамилию").isLength({min : 2}).isString(),
 ]
